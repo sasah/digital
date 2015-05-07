947ce7291ea76bb002c29763f6b6dceb91517d13
sasah <sasahohol2000@gmail.com>
2015-05-08 02:11:57 +0300

    Initial commit

---
+++ install.bat
@@ -0,0 +1,3 @@
+call npm install -g grunt-cli
+call npm install -g bower
+call npm install -g tsd
--- update.bat
+++ update.bat
@@ -1,1 +1,4 @@
-mvn versions:update-parent versions:update-properties
+call mvn versions:update-parent versions:update-properties
+call npm update
+call bower update
+call tsd update -s
