cfbcd30500dd1181839523529918174cf275c844
sasah <sasahohol2000@gmail.com>
2015-05-12 03:03:59 +0300

    angular-bootstrap

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="angularjs/angular-animate.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
